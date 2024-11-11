93a3e92dab0a753daa69a0b12cf77e28d894fac9
Uthando <uthandomkhize@gmail.com>
2024-11-11 22:56:57 +0200

    fixed errors

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const validator = require("validator");
 
 module.exports.register = async (req, res) => {
   const { username, name, surname, accountNumber, IDNumber, password } =
